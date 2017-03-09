2dae3a1ea0206432710f66a78c78e130f7a1332a
Garriga <garrigasbd@yahoo.com>
2017-03-09 01:27:51 +0100

    Delete flowDist.py

--- simulations/simGen/inutil/flowDist.py
+++
@@ -1,9 +0,0 @@
-
-def linear(t,tin,tmax,maxflow):
-	maxflowposs = 180 # vehicles per minute
-	flowparameter = float(maxflow)/maxflowposs
-	if (t < tin):
-		return 0
-	if (t > tmax):
-		return float(flowparameter)
-	return float((t-tin))/(tmax-tin)*flowparameter
