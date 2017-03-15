9f617d20bc8a2fbac8700dd4c37282004cbad6ff
Garriga <garrigasbd@yahoo.com>
2017-03-15 19:02:49 +0100

    Functions to generate train and test csv tables

---
+++ analysis/preprocess/getDataFiles.R
@@ -0,0 +1,57 @@
+library(plyr)
+library(tictoc)
+#preprocesses the data
+getDetectors <- function(type, case, maxDur, seed) {
+  if (type == 'train') {
+    fileName <- paste('output/train/csv/detectors/detectors', case, 'm', maxDur, 's', seed, '.csv', sep = '')
+  } else if (type == 'test') {
+    fileName <- paste('output/test/detectors/detectors', case, 'm', maxDur, 's', seed, '.csv', sep = '')
+  }
+  detectors <- read.csv(file = fileName, sep = ";")
+  names(detectors) <- substring(names(detectors), 10)
+  
+  #extracts edge id (we have lane_id)
+  extractEdge <- function(s) {
+    pos <- regexpr("_", s)
+    return(substr(s,1,pos-1))
+  }
+  detectors$id <- as.character(lapply(detectors$id, extractEdge))
+  
+  #extract the only relevant variables for us
+  detectors <- subset(detectors, select = c(begin, id, nVehContrib))
+  
+  #we merge data from lanes to get edge data
+  merge <- function(df) {
+    df$nVehContrib[1] <- sum(df$nVehContrib)
+    return(df[1,])
+  }
+  edges.data <- ddply(detectors, .(begin, id), merge)
+  
+  #list with the data of every detector
+  edges.data <- dlply(edges.data, .(id))
+  
+  getDf <- function(s) {
+    df <- do.call(cbind, lapply(edges.data, function(df) return(df[names(df) == s])))
+    names(df) <- names(edges.data)
+    return(df)
+  }
+  var <- 'nVehContrib'
+  df <- getDf(var)
+  #we remove the first 30 min of simulation because it is not a stationary phase (1 row = 1 min)
+  df <- df[31:nrow(df),]
+  df$case <- paste('case', case, sep = '')
+  return(df)
+}
+
+args <- commandArgs(trailingOnly = TRUE)
+type <- args[1]
+seed <- args[2]
+maxDur <- args[3]
+ncases <- as.numeric(args[4])
+if (type == 'train') {
+  df <- do.call(rbind, llply(0:(ncases-1), function(x) getDetectors(type, x, maxDur, seed)))
+} else if (type == 'test') {
+  df <- do.call(rbind, llply(0:(ncases-1), function(x) getDetectors(type, x, maxDur, seed)))
+}
+fileOut = paste('output/', type, '/', type, '_m', maxDur, 's', seed, '.csv', sep = '')
+write.table(df, file = fileOut, sep = ";", quote = FALSE, row.names = FALSE) 
